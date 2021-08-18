7a7a32bc24bcb87bef53e63680552ea7b91f9ccb
Marko Babović <mb20170119@student.fon.bg.ac.rs>
2021-08-18 14:56:18 +0200

    Dodat projekat Data i kreirana baza i migracija !

---
+++ Domen/Migrations/20210818124924_kreirane baz.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Domen.Migrations
+{
+    public partial class kreiranebaz : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Korisnik",
+                columns: table => new
+                {
+                    KorisnikId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Sifra = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TipKorisnika = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Korisnik", x => x.KorisnikId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Let",
+                columns: table => new
+                {
+                    LetId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MestoPolaska = table.Column<int>(type: "int", nullable: false),
+                    MestoDolaska = table.Column<int>(type: "int", nullable: false),
+                    Datum = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    BrojPresedanja = table.Column<int>(type: "int", nullable: false),
+                    BrojMesta = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Let", x => x.LetId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rezervacija",
+                columns: table => new
+                {
+                    KorisnikId = table.Column<int>(type: "int", nullable: false),
+                    LetId = table.Column<int>(type: "int", nullable: false),
+                    StatusLeta = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rezervacija", x => new { x.LetId, x.KorisnikId });
+                    table.ForeignKey(
+                        name: "FK_Rezervacija_Korisnik_KorisnikId",
+                        column: x => x.KorisnikId,
+                        principalTable: "Korisnik",
+                        principalColumn: "KorisnikId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Rezervacija_Let_LetId",
+                        column: x => x.LetId,
+                        principalTable: "Let",
+                        principalColumn: "LetId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rezervacija_KorisnikId",
+                table: "Rezervacija",
+                column: "KorisnikId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Rezervacija");
+
+            migrationBuilder.DropTable(
+                name: "Korisnik");
+
+            migrationBuilder.DropTable(
+                name: "Let");
+        }
+    }
+}
